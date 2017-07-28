2ed03ac3d568e03af1ae95411ce90f45cf03ed66
NiYa193 <1556579665@qq.com>
2017-07-28 22:38:31 +0800

    修改了BroadcastTest
    修改该项目，让其具有接收广播的优先级，并对广播进行截断，使第二个程序无法再接收到广播

--- chapterFive/BroadcastTest/app/src/main/java/top/fcc143/broadcasttest/MainActivity.java
+++ chapterFive/BroadcastTest/app/src/main/java/top/fcc143/broadcasttest/MainActivity.java
@@ -35,7 +35,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v){
                 Intent intent = new Intent("com.example.broadcasttest.MY_BROADCAST");
-                sendBroadcast(intent);
+                sendOrderedBroadcast(intent, null);
             }
         });
 
